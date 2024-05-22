b9cacdc8ceed48dcd3dfaa62740f3f89f6fe296d
shujabakhtiar <47808000+shujabakhtiar@users.noreply.github.com>
2024-05-22 22:36:19 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,18 +6,38 @@ export const Home = () => {
     <div className='full-container  flex justify-center items-center bg-red-400'>
         Video
     </div>
+    
     <div className='full-container flex justify-center items-center'>
-        Caption 1 left
-    </div>
-    <div className='full-container flex justify-center items-center'>
-        Caption 2 right
-    </div>
-    <div className='full-container flex justify-center items-center'>
-        <div className='bg-orange-500 h-full container'>
-            Featured Projects
+        <div className='container md:h-5/6 flex-col flex md:relative md:flex-row'>
+            <div className='md:absolute md:top-0 md:left-0'>
+                Text here
+            </div>
+            <div className='md:absolute md:bottom-0 md:right-0'>
+                Image here
+            </div>
         </div>
     </div>
-    <div className='full-container container flex justify-center items-center'>
+
+    <div className='full-container flex justify-center items-center'>
+        <div className='container md:h-5/6 flex flex-col items-center md:flex md:flex-row md:justify-around'>
+            <div className=''>
+                Text here
+            </div>
+            <div className=''>
+                Image here
+            </div>
+        </div>
+    </div>
+
+    <div className='full-container flex justify-start flex flex-col'>
+        <div className='container w-full text-left'>
+            Featured Projects
+        </div>
+        <div className='w-full bg-red-300'>
+            CARDS
+        </div>
+    </div>
+    <div className='container flex justify-center items-center'>
         Footer 
     </div>
     </>
